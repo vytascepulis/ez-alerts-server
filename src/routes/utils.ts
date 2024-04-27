ffec5f2e8cd4e9b604304ddb9901333bfd9a5f59
Vytas <cepulis.vytas@gmail.com>
2024-04-27 19:28:19 +0300

    updated purge function

--- src/routes/utils.ts
+++ src/routes/utils.ts
@@ -4,9 +4,13 @@ import { FileModel } from '../models/file';
 const router = express.Router();
 
 router.post('/purge', async (req: Request, res: Response) => {
-  await UserModel.collection.drop();
-  await FileModel.collection.drop();
-  res.send('Collections dropped');
+  try {
+    await UserModel.collection.drop();
+    await FileModel.collection.drop();
+    res.status(200).json({ message: 'Collections dropped' });
+  } catch (e) {
+    res.status(500).json({ message: 'Error dropping collection' });
+  }
 });
 
 export default router;
